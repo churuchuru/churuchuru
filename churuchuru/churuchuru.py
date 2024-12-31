6be53e414dd4ab8e68ed141fdbb9337f002e5135
ritchieng <ritchieng@u.nus.edu>
2024-12-31 18:51:44 +0800

    Add menu

--- churuchuru/churuchuru.py
+++ churuchuru/churuchuru.py
@@ -1,6 +1,61 @@
 import reflex as rx
 
 '''
+Main Application
+'''
+
+# Menu bar
+def layout(*children):
+    return rx.box(
+        rx.vstack(
+            # Navigation bar
+            rx.hstack(
+                rx.heading("My App"),
+                rx.hstack(
+                    rx.link("Home", href="/"),
+                    rx.link("Counter", href="/counter"),
+                    spacing="3",
+                ),
+                width="100%",
+                justify="between",
+                padding="4",
+            ),
+            # Main content
+            rx.box(
+                *children,
+                padding_top="6em",
+            ),
+        )
+    )
+
+# Main page
+def index():
+    return layout(
+        rx.center(
+        rx.flex(
+            rx.card(
+                rx.link(
+                    rx.vstack(
+                        rx.heading("Counter"),
+                        rx.text("Application to add or minus 1 or reset counter"),
+                        spacing="2",
+                        align="center",
+                    ),
+                    href="/counter"
+                ),
+                as_child=True,
+            ),
+            spacing="2",
+            justify="center",
+            align="center",
+            flex_wrap="wrap",
+            width="100%",
+        ),
+        height="100vh",
+    )
+    )
+
+'''
 Counter Application
 '''
 
@@ -21,11 +76,11 @@ class State(rx.State):
         self.count = 0
 
 
-# Frontend
-def index() -> rx.Component:
+# Counter App
+def counter() -> rx.Component:
     return rx.center(
         rx.vstack(
-            rx.heading("Counter App"),
+            rx.heading("Counter"),
             rx.hstack(
                 rx.button(
                     "Decrement", 
@@ -51,7 +106,13 @@ def index() -> rx.Component:
         height="100vh",
     )
 
+
+'''
+Run page
+'''
 # Define application
 app = rx.App()
 # Add pages
 app.add_page(index)
+app.add_page(counter)
+
